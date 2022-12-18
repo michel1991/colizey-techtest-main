0075d4340c1f1a81750952a4039b7f17c93bd91e
msmvoumamvouma <56298431+msmvoumamvouma@users.noreply.github.com>
2022-12-18 14:45:15 +0100

    build: add file-loader to load image during build

--- webpack.config.js
+++ webpack.config.js
@@ -66,6 +66,12 @@ Encore
 
     // enables Sass/SCSS support
     //.enableSassLoader()
+  .copyFiles([
+      {
+      from: './assets/images',
+      to: 'images/[path][name].[hash:8].[ext]',
+    }
+  ])
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
